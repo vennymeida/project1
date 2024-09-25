fabc4eef404bd2d6d5b3cbbc84b594d6a40346b5
vennymeida <vnymeida@gmail.com>
2024-09-25 07:55:52 +0700

    first commit

---
+++ app/Http/Controllers/RumahController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rumah;
+use App\Http\Requests\StoreRumahRequest;
+use App\Http\Requests\UpdateRumahRequest;
+
+class RumahController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRumahRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rumah $rumah)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Rumah $rumah)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRumahRequest $request, Rumah $rumah)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rumah $rumah)
+    {
+        //
+    }
+}
